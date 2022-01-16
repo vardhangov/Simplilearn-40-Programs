1973d6079f76bc82c12b1212c4e25af34d3a18e6
bandarugovardha <bandarugovardha@ip-172-31-85-58.ec2.internal>
2022-01-16 08:03:38 +0000

    Array Link First commit

---
+++ ArrayLink/src/com/array/ArrayLink.java
@@ -0,0 +1,26 @@
+package com.array;
+
+import java.util.Scanner;
+
+public class ArrayLink {
+	void arrayValues(int b[]) {
+	   	 
+	   	 System.out.println(b[0]);
+	   	 
+	    }
+	}
+	class ExMain{
+	    public static void main(String[] args) {
+	   	 int a[]=new int[5];
+	   	 Scanner sc=new Scanner(System.in);
+	   	 System.out.println("enter the input values");
+	   	 
+	   	 for(int i=0;i<a.length;i++) {
+	   		 a[i]=sc.nextInt();
+	   	 }
+	   	 
+	   	 ArrayLink al=new ArrayLink();
+	   	 al.arrayValues(a); 
+	   	 
+	    }
+}
